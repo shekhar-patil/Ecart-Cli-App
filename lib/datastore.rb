613439cfb8396ea6fc7fb7c65159bef3301f3ec9
shekhar-patil <patilshekhar900@gmail.com>
2020-05-17 12:01:06 +0530

    app is ready to write test cases in test enviroment

--- lib/datastore.rb
+++ lib/datastore.rb
@@ -4,8 +4,14 @@ require './lib/helper'
 class Datastore
   extend ECart::Helper
 
+  #this to handle testing of code.
+  @database_folder = 'database/' # will change this class variable to test_db while testing
+  class << self
+    attr_accessor :database_folder # create database_folder class level variable and create getter and setter
+  end
+
   def self.create_record(object, obj_type)
-    store = PStore.new("#{obj_type}.pstore")
+    store = PStore.new("#{@database_folder}#{obj_type}.pstore")
     store.transaction do
       store[object.id] = object
     end
@@ -13,7 +19,7 @@ class Datastore
   self.singleton_class.send(:alias_method, :update_record, :create_record)
 
   def self.fetch_records(params, obj_type)
-    store = PStore.new("#{obj_type}.pstore")
+    store = PStore.new("#{@database_folder}#{obj_type}.pstore")
     objects = []
     store.transaction(true) do
       store.roots.each do |obj|
@@ -24,21 +30,21 @@ class Datastore
   end
 
   def self.current_user_id
-    store = PStore.new("session.pstore")
+    store = PStore.new("#{@database_folder}session.pstore")
     store.transaction do
       store['current_user']
     end
   end
 
   def self.create_session(user_id)
-    store = PStore.new('session.pstore')
+    store = PStore.new("#{@database_folder}session.pstore")
     store.transaction do
       store['current_user'] = user_id
     end
   end
 
   def self.delete_session
-    store = PStore.new("session.pstore")
+    store = PStore.new("#{@database_folder}session.pstore")
     store.transaction do
       store.delete('current_user')
     end
@@ -46,7 +52,7 @@ class Datastore
 
   def self.fetch_resource(id, obj_type)
     load("./lib/#{obj_type.to_s}.rb")
-    store = PStore.new("#{obj_type}.pstore")
+    store = PStore.new("#{@database_folder}#{obj_type}.pstore")
     store.transaction do
       store[id]
     end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,17 @@
 require "bundler/setup"
 require "e_cart"
+require './lib/datastore.rb'
 
 RSpec.configure do |config|
+  # create new database each time rspec is running
+  system('rm -r test_db') # drop previous test_db
+  system('mkdir test_db') # create new test_db
+
+  ENV['env'] = 'testing' # this is to check environment in the app.
+
+  # change database folder for testing
+  Datastore.database_folder = 'test_db'
+
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
 
